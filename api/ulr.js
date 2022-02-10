2a377ca663125c9d076606867ea8081f33ff41a0
David Charif <davidcharif@hotmail.com>
2022-02-09 23:00:40 -0500

    cat images

--- api/ulr.js
+++ api/ulr.js
@@ -3,5 +3,5 @@
 // export const urlGatos = 'http://localhost:3001/gatos/'
 
 export const urlPerros = 'https://gist.githubusercontent.com/DavidCharif/35dd153961a69ef73734ff0233ff89ee/raw/5321b12b233e7f818f3df54afca1f5f3bee9f198/perros.json'
-export const urlGatos = 'https://gist.githubusercontent.com/DavidCharif/45b5b42f577dfb6551da5d8572ff0a7a/raw/7ffb941b017e7efb9b316d6e47cc1a45355d297c/gatos.json'
+export const urlGatos = 'https://gist.githubusercontent.com/DavidCharif/45b5b42f577dfb6551da5d8572ff0a7a/raw/4f435a1d114c4db86772ce1f556cb733750f7761/gatos.json'
 
